cb27d03197bcee465a098dbb338eb5f47b5541cb
ldruitt <42873492+ldruitt@users.noreply.github.com>
2020-03-10 11:25:46 -0400

    Add files via upload

--- kerasmodel.py
+++ kerasmodel.py
@@ -11,8 +11,8 @@ def main():
     Xtrain = np.load('MNISTXtrain1.npy')
     ytrain = np.load('MNISTytrain1.npy')
     imageindex  = 7777
-    print(ytrain[imageindex])
-    plt.imshow(Xtrain[imageindex], cmap='Greys')
+    #print(ytrain[imageindex])
+    #plt.imshow(Xtrain[imageindex], cmap='Greys')
     #print(Xtrain.shape)
     # Reshaping the array to 4-dims so that it can work with the Keras API
     Xtrain = Xtrain.reshape(Xtrain.shape[0], 28, 28, 1)
@@ -28,6 +28,27 @@ def main():
     print('Number of images in x_train', Xtrain.shape[0])
     print('Number of images in x_test', Xtest.shape[0])
 
+    # Model 1: Basic 2-hidden-layer, based on theoretical minimum layer needed to solve any problem. Ignores dimensions.
+    model1 = Sequential()
+    model1.add(Flatten())
+    model1.add(Dense(128))
+    model1.add(Dense(10))
+
+    model1.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model1.fit(x=Xtrain, y=ytrain, epochs=10)
+
+    # Model 2: Intermediate model using 2D layers for better image comprehension. Like model 3, without activations.
+    model2 = Sequential()
+    model2.add(Conv2D(28, kernel_size=(3, 3), input_shape=input_shape))
+    model2.add(MaxPooling2D(pool_size=(2, 2)))
+    model2.add(Flatten())
+    model2.add(Dense(128))
+    model2.add(Dense(10))
+
+    model2.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model2.fit(x=Xtrain, y=ytrain, epochs=10)
+
+    # Model 3: Loren's model. Adds activation functions and a dropout layer to reduce potential overfitting.
     model = Sequential()
     model.add(Conv2D(28, kernel_size=(3, 3), input_shape=input_shape))
     model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -36,13 +57,33 @@ def main():
     model.add(Dropout(0.2))
     model.add(Dense(10, activation=tf.nn.softmax))
 
-    model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     model.fit(x=Xtrain, y=ytrain, epochs=10)
 
-    model.evaluate(Xtest, ytest)
+    # Model 4: High complexity model with multiple 2D layer groups. Testing increased complexity.
+    model4 = Sequential()
+    model4.add(Conv2D(28, kernel_size=(3, 3), input_shape=input_shape))
+    model4.add(MaxPooling2D(pool_size=(2, 2)))
+    model4.add(Conv2D(28, kernel_size=(3, 3)))
+    model4.add(MaxPooling2D(pool_size=(2, 2)))
+    model4.add(Flatten())
+    model4.add(Dense(128, activation=tf.nn.relu))
+    model4.add(Dropout(0.2))
+    model4.add(Dense(10, activation=tf.nn.softmax))
 
+    model4.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model4.fit(x=Xtrain, y=ytrain, epochs=10)
+
+    # Evaluate the models.
+    score1, acc1 = model1.evaluate(Xtest, ytest)
+    score2, acc2 = model2.evaluate(Xtest, ytest)
+    score3, acc3 = model.evaluate(Xtest, ytest)
+    score4, acc4 = model4.evaluate(Xtest, ytest)
+
+    print("Model 1 score/accuracy: " + str(score1) + ", " + str(acc1))
+    print("Model 2 score/accuracy: " + str(score2) + ", " + str(acc2))
+    print("Model 3 score/accuracy: " + str(score3) + ", " + str(acc3))
+    print("Model 4 score/accuracy: " + str(score4) + ", " + str(acc4))
 
 if __name__ == '__main__':
     main()
