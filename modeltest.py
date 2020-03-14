4412cf5dfabfbacc5a156c2da604c2003ed41348
ldruitt <42873492+ldruitt@users.noreply.github.com>
2020-03-14 16:04:27 -0400

    Add files via upload

--- modeltest.py
+++ modeltest.py
@@ -3,7 +3,6 @@ import sys
 import tensorflow as tf
 
 def main():
-    print('loading arg')
     X = np.load(sys.argv[1])
     # Reshaping the array to 4-dims so that it can work with the Keras API
     X = X.reshape(X.shape[0], 28, 28, 1)
@@ -12,11 +11,11 @@ def main():
     X = X.astype('float32')
     # Normalizing the RGB codes by dividing it to the max RGB value.
     X /= 255
-    print("Loading Model")
     model = tf.keras.models.load_model('MyBestModel_00956434_01023420.h5', custom_objects={'softmax_v2': tf.nn.softmax})
-    print('run')
     y = model.predict_classes(X)
-    print(y)
+    # checking the accuracy compared to ytrain: 0.9980333333333333
+    # ytrain = np.load('MNISTytrain1.npy')
+    # print(np.sum(y == ytrain)/y.size)
     outputString = "\n".join(map(str,y))
     outputFile = open('output.txt', 'w')
     outputFile.write(outputString)
